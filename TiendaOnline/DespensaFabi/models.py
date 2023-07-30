c0ea652dc7e670868f53759551e2414fe7d036f2
Fabian O. Frias <fabianoscarfrias@gmail.com>
2023-07-30 13:15:33 -0300

    Nav-Bar2.0 95%

--- TiendaOnline/DespensaFabi/models.py
+++ TiendaOnline/DespensaFabi/models.py
@@ -22,6 +22,7 @@ class Producto(models.Model):
     precio = models.DecimalField(max_digits=10, decimal_places=2)
     stock = models.PositiveIntegerField()
     categoria = models.CharField(max_length=20, choices=CATEGORIAS_CHOICES, default='comestibles')
+    image = models.ImageField(upload_to='productos', null=True, blank=True)
 
     def __str__(self):
         return f'{self.nombre} - {self.precio}'
